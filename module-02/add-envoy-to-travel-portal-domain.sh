64f65c119333c94706d9612791ae880ca1395916
Stelios Kousouris <skousou@gmail.com>
2026-04-02 15:55:08 +0100

    Update add-envoy-to-travel-portal-domain.sh
    adjust travel portal istio-proxy resources

--- module-02/add-envoy-to-travel-portal-domain.sh
+++ module-02/add-envoy-to-travel-portal-domain.sh
@@ -8,8 +8,19 @@ echo
 USERNAME=$(oc whoami)
 
 oc patch deployment/travels --type=merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n ${USERNAME}-travel-portal
+oc patch deployment/travels --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/proxyCPU": "100m"}}}}}' -n ${USERNAME}-travel-portal
+oc patch deployment/travels --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/proxyCPULimit": "200m"}}}}}' -n ${USERNAME}-travel-portal
+oc patch deployment/travels --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/proxyMemoryLimit": "300Mi"}}}}}' -n ${USERNAME}-travel-portal
+
 oc patch deployment/viaggi --type=merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n ${USERNAME}-travel-portal
+oc patch deployment/viaggi --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/proxyCPU": "100m"}}}}}' -n ${USERNAME}-travel-portal
+oc patch deployment/viaggi --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/proxyCPULimit": "200m"}}}}}' -n ${USERNAME}-travel-portal
+oc patch deployment/viaggi --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/proxyMemoryLimit": "300Mi"}}}}}' -n ${USERNAME}-travel-portal
+
 oc patch deployment/voyages --type=merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n ${USERNAME}-travel-portal
+oc patch deployment/voyages --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/proxyCPU": "100m"}}}}}' -n ${USERNAME}-travel-portal
+oc patch deployment/voyages --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/proxyCPULimit": "200m"}}}}}' -n ${USERNAME}-travel-portal
+oc patch deployment/voyages --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/proxyMemoryLimit": "300Mi"}}}}}' -n ${USERNAME}-travel-portal
 
 echo
 echo
